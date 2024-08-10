604063b69085bb3bdab26595d10283e6d64bfd01
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-08-10 14:10:28 +1200

    refactor: Refactored button to adhere to SonarLint rules.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,17 +7,15 @@ export default function index() {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <>
+    <button
+      className={styles.button}
+      onClick={() => {
+        setIsActive(!isActive);
+      }}
+    >
       <div
-        onClick={() => {
-          setIsActive(!isActive);
-        }}
-        className={styles.button}
-      >
-        <div
-          className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
-        ></div>
-      </div>
-    </>
+        className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
+      ></div>
+    </button>
   );
 }
